b387a1614fe81a02b8fe4ce1b9651ecafe3b7c42
jacobjolibois <jacobjolibois@hey.com>
2022-02-07 14:08:51 -0600

    finished the fetch single user by id endpoint

--- api/server.js
+++ api/server.js
@@ -21,7 +21,7 @@ server.post('/api/users', (req, res) => {
 			});
 });
 
-// | GET    | /api/users     | Returns an array users.                                                                                |
+// ✅ | GET    | /api/users     | Returns an array users.                                                                                |
 server.get('/api/users', (req, res) => {
 	users.find()
 		.then(users => {
@@ -32,14 +32,20 @@ server.get('/api/users', (req, res) => {
 		});
 });
 
-// | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
+// ✅ | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
 server.get('/api/users/:id', (req, res) => {
-
+	users.findById(req.params.id)
+		.then(user => {
+			res.json(user);
+		})
+		.catch(() => {
+			res.status(500).json({message: 'Could not find user.'});
+		});
 });
 
 // | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
 server.delete('/api/users/:id', (req, res) => {
-
+	
 });
 
 // | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |

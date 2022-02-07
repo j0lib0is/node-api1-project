d4828b907ffee1a10de9607cce53e06efb8a6bff
jacobjolibois <jacobjolibois@hey.com>
2022-02-07 16:12:35 -0600

    completed validation for the PUT endpoint

--- api/server.js
+++ api/server.js
@@ -26,24 +26,24 @@ server.get('/api/users', (req, res) => {
 		.then(users => {
 			res.json(users);
 		})
-		.catch(() => {
+		.catch((err) => {
 			res.status(500).json({message: 'The users information could not be retrieved'});
 		});
 });
 
 // ✅ | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
-server.get('/api/users/:id', (req, res) => {
-	users.findById(req.params.id)
-		.then(returnedUser => {
-			if (returnedUser === null) {
-				res.status(404).json({message: 'The user with the specified ID does not exist'});
-			} else {
-				res.json(returnedUser);
-			}
-		})
-		.catch(() => {
-			res.status(500).json({message: 'The user information could not be retrieved'});
-		});
+server.get('/api/users/:id', async (req, res) => {
+	try {
+		const searchedUser = await users.findById(req.params.id);
+
+		if (searchedUser == null) {
+			res.status(404).json({message: 'The user with the specified ID does not exist'});
+		} else {
+			res.json(searchedUser);
+		}
+	} catch(err) {
+		res.status(500).json({message: 'The user information could not be retrieved'});
+	};
 });
 
 // ✅ | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
@@ -56,25 +56,27 @@ server.delete('/api/users/:id', (req, res) => {
 				res.json(removedUser);
 			}
 		})
-		.catch(() => {
+		.catch((err) => {
 			res.status(500).json({message: 'The user could not be removed'});
 		});
 });
 
 // ✅ | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
 server.put('/api/users/:id', async (req, res) => {
-	try {
-		const updatedUser = await users.update(req.params.id, req.body);
-		if (updatedUser === null) {
-			res.status(404).json({message: 'Could not find user.'});
-			return;
-		} else {
-			res.json(updatedUser);
+	if (!req.body.name || !req.body.bio) {
+		res.status(400).json({message: 'Please provide name and bio for the user'});
+	} else {
+		try {
+			const updatedUser = await users.update(req.params.id, req.body);
+			if (updatedUser === null) {
+				res.status(404).json({message: 'The user with the specified ID does not exist'});
+			} else {
+				res.json(updatedUser);
+			}
+		} catch(err) {
+			res.status(500).json({message: 'The user information could not be modified'});
 		}
-	}
-	catch(e) {
-		res.status(500).json({message: 'Could not update user.'});
-	}
+	};
 });
 
 
